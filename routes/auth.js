eec8a72510c7a1be780ccde406b2e30cf80497c0
RafaV12 <rafaelv1926@gmail.com>
2022-05-05 21:17:36 -0400

    Set up MongoDB and auth route

---
+++ routes/auth.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+const {
+  login,
+  register,
+} = require("../controllers/auth");
+
+router.get("/register", register);
+router.get("/login", login);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,30 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
 const app = express();
 
+const connectDB = require('./db/connect');
+
 app.use(express.json({ limit: '50mb' }));
 app.use(cors());
 
-const PORT = 3001;
+// Routers
+const authRoute = require('./routes/auth')
 
-app.listen(PORT, () => console.log(`Server is listening PORT ${PORT}...`));
+// Routes
+
+app.use('/', authRoute);
+
+const PORT = process.env.PORT || 3001;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(PORT, () => console.log(`Server is listening PORT ${PORT}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
